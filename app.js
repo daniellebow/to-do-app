3c52080d338a62d90c6cf588bbd0379c58b443d2
danielle bowers <daniellebowers@danielles-MacBook-Pro.local>
2017-10-02 16:00:05 -1000

    Assignment 8 completed

---
+++ app.js
@@ -0,0 +1,53 @@
+
+function onReady() {
+     var toDos = [];
+     var addToDoForm = document.getElementById('addToDoForm');
+     function createNewToDo() {
+       var newToDoText = document.getElementById('newToDoText');
+       toDos.push({
+       title: newToDoText.value,
+       complete: false
+       });
+       newToDoText.value = '';
+           renderTheUI(toDos);
+     }
+
+     function renderTheUI(toDos) {
+       var todoList = document.getElementById('toDoList');
+
+       toDoList.innerHTML = '';
+
+       toDos.forEach(function(toDo) {
+       var newLi = document.createElement('li');
+       var checkbox = document.createElement('input');
+       var deleteButton = document.createElement('button');
+       checkbox.type = "checkbox";
+       deleteButton.type="button";
+
+       newLi.innerHTML = toDo.title;
+
+       todoList.appendChild(newLi);
+       newLi.appendChild(checkbox);
+       newLi.appendChild(deleteButton);
+
+       deleteButton.addEventListener('click', function() {
+                                   deleteButton.parentNode.remove();
+                           });
+
+   });
+
+     }
+
+
+     addToDoForm.addEventListener('submit', function(event) {
+       event.preventDefault();
+       createNewToDo();
+});
+
+  renderTheUI(toDos);
+
+  }
+
+window.onload = function() {
+  onReady();
+};
